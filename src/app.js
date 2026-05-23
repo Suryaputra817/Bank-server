cf5aca97c5cb95330334333561736749fd1f644e
Manjit Kumar Shyanti <manjitkumarshyanti@gmail.com>
2026-05-23 19:14:42 +0530

    feat: add root endpoint to confirm server status

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ import transactionRouter from "./routes/transaction.routes.js";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.get("/", (req, res) => {
+  res.send("Ledger service is up and running");
+});
 app.use("/api/auth", authRouter);
 app.use("/api/accounts", accountRouter);
 app.use("/api/transactions", transactionRouter);
